4b2b2f85c4e641f4914a9ec6a943da3607f8e920
arkosh <trukan24@gmail.com>
2022-08-17 17:24:01 +0300

    refactored error handling

--- main.go
+++ main.go
@@ -1,42 +1,42 @@
 package main
 
 import (
-	"fmt"
 	"github.com/Arkosh744/urlbae_client_API/urlbae"
 	"log"
 	"time"
 )
 
-const apiToken = "PASTE_YOUR_API_TOKEN_HERE"
+const apiKey = "PASTE_YOUR_API_TOKEN_HERE"
 
 func main() {
 	// Create a new client
-	client, err := urlbae.NewClient(apiToken, time.Second*5)
-	urlbae.ErrorFatalEnding(err)
-
-	// Get account info and check login status
-	message := urlbae.GetAccountInfo(client)
-	if message.Message == urlbae.InvalidKeyAPI {
-		log.Fatalln("Invalid API key")
-	} else {
-		fmt.Print("Account info:\n" +
-			"Email: " + message.AccountInfo.Email + "\n" +
-			"Username: " + message.AccountInfo.Username + "\n" +
-			"Status: " + message.AccountInfo.Status + "\n" +
-			"Registered at: " + message.AccountInfo.RegisteredAt + "\n")
+	client, err := urlbae.NewClient(apiKey, time.Second*5)
+	if err != nil {
+		log.Fatalf("Error creating client: %s", err)
 	}
 
+	// Get account info and check login status
+	message, err := urlbae.GetAccountInfo(client)
+	log.Print("Account info:\n" +
+		"Email: " + message.Email + "\n" +
+		"Username: " + message.Username + "\n" +
+		"Status: " + message.Status + "\n" +
+		"Registered at: " + message.RegisteredAt + "\n")
+
 	// Write down your link data
-	longUrlname := "https://www.google.ru"
-	customName := "goodsagle"
+	longUrlname := "https://www.yandex.ru"
+	customName := "yandex"
 	expirationDate := time.Now().Add(time.Hour * 24) // set to 1 day from now
 
-	UrlToShort := &urlbae.LongLinkData{longUrlname, customName, expirationDate}
+	UrlToShort := &urlbae.LongLinkData{LongURL: longUrlname, CustomName: customName, ExpirationDate: expirationDate}
 
 	// Shorten your url
 	urlbae.DoShortLink(client, UrlToShort)
 
 	// Get List of all active short links
-	urlbae.GetAllLinks(client)
+	_, err = urlbae.GetAllLinks(client)
+	if err != nil {
+		log.Println(err)
+	}
 
 }
--- urlbae/client.go
+++ urlbae/client.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"encoding/json"
 	"errors"
-	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
@@ -20,7 +19,6 @@ type client struct {
 }
 
 func NewClient(apiKey string, timeout time.Duration) (*client, error) {
-
 	if timeout == 0 {
 		return nil, errors.New("timeout can't be 0")
 	}
@@ -33,49 +31,62 @@ func NewClient(apiKey string, timeout time.Duration) (*client, error) {
 }
 
 func DoShortLink(client *client, LongLinkData *LongLinkData) string {
-	fmt.Println("Shortening the url: " + LongLinkData.LongURL + " with the alias: " + LongLinkData.CustomName + " and expiration date: " + LongLinkData.ExpirationDate.Format("2006-01-02 15:04:05"))
-	ShortedLink := shortLink(client, LongLinkData)
+	log.Println("Shortening the url: " + LongLinkData.LongURL + " with the alias: " + LongLinkData.CustomName + " and expiration date: " + LongLinkData.ExpirationDate.Format("2006-01-02 15:04:05"))
+	ShortedLink, _ := shortLink(client, LongLinkData)
 
+	// If alias exists - we generate new random alias
 	if ShortedLink.Message == CustomNameExists {
-		fmt.Println("Sorry, that alias is taken. We generated random one.")
+		log.Println("Sorry, that alias is taken. We generated random one.")
 		LongLinkData.CustomName = ""
-		fmt.Println("Shortening the url: " + LongLinkData.LongURL + " without alias and expiration date: " + LongLinkData.ExpirationDate.Format("2006-01-02 15:04:05"))
-		ShortedLink = shortLink(client, LongLinkData)
+		log.Println("Shortening the url: " + LongLinkData.LongURL + " without alias and expiration date: " + LongLinkData.ExpirationDate.Format("2006-01-02 15:04:05"))
+		ShortedLink, _ = shortLink(client, LongLinkData)
 	}
-	fmt.Println("Your new shorted link is: " + ShortedLink.ShortUrl)
+	log.Println("New shorted link is: " + ShortedLink.ShortUrl)
 	return ShortedLink.ShortUrl
 }
 
-func GetAccountInfo(client *client) LinkResponse {
+func GetAccountInfo(client *client) (AccountInfo, error) {
 	url := "https://urlbae.com/api/account"
-
 	req, err := http.NewRequest("GET", url, nil)
-	ErrorFatalEnding(err)
+	if err != nil {
+		return AccountInfo{}, err
+	}
 
 	req.Header = http.Header{
 		"Authorization": {"Bearer " + client.apiKey},
 		"Content-Type":  {"application/json"},
 	}
-
 	res, err := client.client.Do(req)
-	ErrorFatalEnding(err)
+	if err != nil {
+		return AccountInfo{}, err
+	}
 
 	defer func(Body io.ReadCloser) {
 		err := Body.Close()
-		ErrorFatalEnding(err)
+		if err != nil {
+			log.Printf("Error closing body: %s", err)
+		}
 	}(res.Body)
 
 	body, err := ioutil.ReadAll(res.Body)
-	ErrorFatalEnding(err)
+	if err != nil {
+		return AccountInfo{}, err
+	}
 
 	AccountDataResponse := LinkResponse{}
 	err = json.Unmarshal(body, &AccountDataResponse)
-	ErrorFatalEnding(err)
+	if err != nil {
+		return AccountInfo{}, err
+	}
 
-	return AccountDataResponse
+	if AccountDataResponse.Message == InvalidKeyAPI {
+		return AccountInfo{}, errors.New("Invalid API key")
+	}
+
+	return AccountDataResponse.AccountInfo, nil
 }
 
-func shortLink(client *client, LongLinkData *LongLinkData) LinkResponse {
+func shortLink(client *client, LongLinkData *LongLinkData) (LinkResponse, error) {
 	url := "https://urlbae.com/api/url/add"
 
 	// if expirationDate is not set, it will be set to 1 day from now
@@ -90,67 +101,74 @@ func shortLink(client *client, LongLinkData *LongLinkData) LinkResponse {
 	}
 
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(data))
-	ErrorFatalEnding(err)
+	if err != nil {
+		return LinkResponse{}, err
+	}
 
 	req.Header = http.Header{
 		"Authorization": {"Bearer " + client.apiKey},
 		"Content-Type":  {"application/json"},
 	}
-
 	res, err := client.client.Do(req)
-	ErrorFatalEnding(err)
+	if err != nil {
+		return LinkResponse{}, err
+	}
 
 	defer func(Body io.ReadCloser) {
 		err := Body.Close()
-		ErrorFatalEnding(err)
+		if err != nil {
+			log.Printf("Error closing body: %s", err)
+		}
 	}(res.Body)
 
 	body, err := ioutil.ReadAll(res.Body)
-	ErrorFatalEnding(err)
+	if err != nil {
+		return LinkResponse{}, err
+	}
 
 	ShortLinkResponseData := LinkResponse{}
 
 	err = json.Unmarshal(body, &ShortLinkResponseData)
-	ErrorFatalEnding(err)
+	if err != nil {
+		return LinkResponse{}, err
+	}
 
-	return ShortLinkResponseData
+	return ShortLinkResponseData, nil
 }
 
-func GetAllLinks(client *client) {
+func GetAllLinks(client *client) ([]GeneratedLinkData, error) {
 	url := "https://urlbae.com/api/urls?order=date"
-
 	req, err := http.NewRequest("GET", url, nil)
-	ErrorFatalEnding(err)
+	if err != nil {
+		return nil, err
+	}
 
 	req.Header = http.Header{
 		"Authorization": {"Bearer " + client.apiKey},
 		"Content-Type":  {"application/json"},
 	}
-
 	res, err := client.client.Do(req)
-	ErrorFatalEnding(err)
+	if err != nil {
+		return nil, err
+	}
 
 	defer func(Body io.ReadCloser) {
 		err := Body.Close()
-		ErrorFatalEnding(err)
+		if err != nil {
+			log.Printf("Error closing body: %s", err)
+		}
 	}(res.Body)
 
 	body, err := ioutil.ReadAll(res.Body)
-	ErrorFatalEnding(err)
+	if err != nil {
+		return nil, err
+	}
 
 	AllLinksData := ListLinksResponse{}
-
 	err = json.Unmarshal(body, &AllLinksData)
-
 	for _, link := range AllLinksData.LinkData.URLS {
 		log.Printf(link.ShortURL + " for " + link.LongURL + " Total clicks:" + link.Clicks)
 	}
+	return AllLinksData.LinkData.URLS, nil
 
-}
-
-func ErrorFatalEnding(err error) {
-	if err != nil {
-		//Handle Error
-		log.Fatalln(err)
-	}
 }
